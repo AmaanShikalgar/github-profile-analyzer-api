826ad71bdc00bd58df524354acf97810fea2e69c
AmaanShikalgar <amaanshikalgar2003@gmail.com>
2026-07-15 01:11:44 +0530

    Enable SSL for TiDB

--- config/db.js
+++ config/db.js
@@ -1,16 +1,14 @@
 const mysql = require("mysql2/promise");
-const dotenv = require("dotenv");
-dotenv.config();
+require("dotenv").config();
 
 const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    port: process.env.DB_PORT,
-    ssl: {
-        rejectUnauthorized: true,
-    },
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+
+  ssl: {},
 });
 
 module.exports = pool;
